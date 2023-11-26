9dbb48bef68131ef8aeec2342165001b026aef63
Abhijeet Singh <93851757+Abhijeet-Codes@users.noreply.github.com>
2023-11-26 13:29:10 +0530

    Add files via upload

---
+++ Interface/interface.js
@@ -0,0 +1,12 @@
+var Person1 = {
+    name: 'Abhijeet Singh',
+    location: 'Bhopal',
+    isProgrammer: true,
+};
+console.log(Person1);
+var Person2 = {
+    name: 'Ruhi Kumari',
+    location: 'chind',
+    isProgrammer: false,
+};
+console.log(Person2);
---
+++ Interface/interface.ts
@@ -0,0 +1,21 @@
+interface Person {
+    name: string;
+    location: string;
+    isProgrammer: boolean;
+}
+
+let Person1: Person = {
+    name: 'Abhijeet Singh',
+    location: 'Bhopal',
+    isProgrammer: true,
+};
+
+console.log(Person1);
+
+let Person2: Person = {
+    name: 'Ruhi Kumari',
+    location: 'chind',
+    isProgrammer: false,
+};
+
+console.log(Person2);
