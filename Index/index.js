c6cae4282a44cd69af431aadeb46347577dad0a9
Abhijeet Singh <93851757+Abhijeet-Codes@users.noreply.github.com>
2023-11-26 13:28:17 +0530

    Add files via upload

---
+++ Index/index.js
@@ -0,0 +1,5 @@
+"use strict";
+let anynullvalue = null;
+console.log("This is a Null Value I am printing here: ", anynullvalue);
+let integervalue = 100;
+console.log("This is a Integer Value I am printing here: ");
---
+++ Index/index.ts
@@ -0,0 +1,32 @@
+let anynullvalue: null = null;
+console.log("This is a Null Value I am printing here: ",anynullvalue);
+
+let integervalue: number = 100;
+console.log("This is a Integer Value I am printing here: ",integervalue);
+
+let floatingvalue: number = 3.142;
+console.log("This is a Floating Value I am printing here: ",floatingvalue);
+
+let stringvalue: string = "Abhijeet Singh";
+console.log("This is a String Value I am printing here: ",stringvalue);
+
+let undefinedvalue: undefined = undefined;
+console.log("This is a String Value I am printing here: ",undefinedvalue);
+
+let booleanvalue: boolean = true;
+console.log("This is a Boolean Value I am printing here: ",booleanvalue);
+
+let anyintegervalue: any = 100;
+console.log("This is a Interger Vlaue I am printing here using any data type: ",anyintegervalue);
+
+let anyfloatingvalue: any = 3.142;
+console.log("This is a Floating Value I am printing here using any data type: ",anyfloatingvalue);
+
+let anystringvalue: any = "Abhijeet Singh";
+console.log("This is a String Value I am printing here using any data type: ",anystringvalue);
+
+let anybooleanvalue: any = false;
+console.log("This is a Boolean Value I am printing here using any data type: ",anybooleanvalue);
+
+let anyundefinedvalue: any = undefined;
+console.log("This is a Undefined Value I am printing here using any data type: ",anyundefinedvalue);
