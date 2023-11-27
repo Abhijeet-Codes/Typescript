e81966f69c46418b626381c847cc1fc1b4f0bd52
Abhijeet Singh <93851757+Abhijeet-Codes@users.noreply.github.com>
2023-11-27 17:21:18 +0530

    Add files via upload

---
+++ Reference/reference.ts
@@ -0,0 +1,3 @@
+let firstname = new String("Abhijeet Singh Thakur");
+console.log("Here the string value printing as an object",firstname);
+console.log("The data type of firstname is: ",typeof firstname);
