20f1475059b4e9389a695668f8676ea56284ac35
Abhijeet Singh <93851757+Abhijeet-Codes@users.noreply.github.com>
2023-11-27 17:22:00 +0530

    Add files via upload

---
+++ Point/point.ts
@@ -0,0 +1,7 @@
+let point1 = { x: 1, y: 1};
+let point2 = point1;
+
+point1.y = 100;
+console.log("Here point1 and point2 refer to the same memory address")
+console.log("Hence the value assigned to point1.y is reflecting to point2.y also: ",point2.y);
+//point 1 and point 2 refer to same address to store pont object.
