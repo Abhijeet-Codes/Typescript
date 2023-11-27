794749ea07f0d07b78e0b4b2788035c5fa710e3e
Abhijeet Singh <93851757+Abhijeet-Codes@users.noreply.github.com>
2023-11-27 17:24:51 +0530

    Add files via upload

---
+++ Primitives/primitives.ts
@@ -0,0 +1,11 @@
+let x = 2;
+let y = 1;
+
+x=y;
+console.log("The value of x is: ",x);
+
+y = 100;
+console.log("The value of y is: ",y);
+
+console.log("Even though y changed to 100, x is containing same as 1")
+console.log("The value of x after assining y value as 100: ",x);
